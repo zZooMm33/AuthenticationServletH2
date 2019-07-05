b708326678b077c989a99fb0320e0b0fcff47dfb
Bezumnuixleb <bezumnui32@gmail.com>
2019-07-05 19:24:55 +0300

    authentification -> authentication

---
+++ src/main/java/Authentication.java
@@ -0,0 +1,46 @@
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet(urlPatterns = "/authentication")
+public class Authentication extends HttpServlet
+{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+    {
+        try
+        {
+        Configuration cfgFreeMarker = new Configuration(Configuration.VERSION_2_3_28);
+
+        cfgFreeMarker.setClassForTemplateLoading(this.getClass(), "/");
+
+        Map<String, Object> freeMarkerMap = new HashMap<>();
+        freeMarkerMap.put("name", "Freemarker");
+        Template temp = cfgFreeMarker.getTemplate("authentication.ftl");
+        String out=this.getServletContext().getContextPath();
+        Writer outFreeMarker = new StringWriter();
+
+        String webAddress=""+req.getScheme()+"://"+req.getServerName()+":"+req.getServerPort()+"/AuthenticationServletH2/";
+
+        temp.process(freeMarkerMap, outFreeMarker);
+
+        resp.getWriter().println(outFreeMarker.toString());//request.getCookies()[0].getValue()
+        resp.setContentType("text/html");//200 302 404
+        } catch (TemplateException e)
+        {
+            e.printStackTrace();
+        }
+
+
+
+    }
+}
---
+++ src/main/resources/authentication.ftl
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>${name}!</title>
+    <style>
+
+    </style>
+</head>
+<body>
+<h2>Hello ${name}!</h2>
+</body>
+</html>
