f41f727bc09270df458b5a09e1536a341127cbc0
zZooMm <mmmoonnn@mail.ru>
2019-07-08 14:12:16 +0300

    Тест БД

---
+++ src/main/java/storage/Impl/StorageImpl.java
@@ -0,0 +1,15 @@
+package storage.Impl;
+
+import storage.UserInStorage;
+
+interface Storage {
+    public boolean addUser(String userName, String userMail, String userInfo, String userPass, String userToken);
+    public UserInStorage getInfoUserByName(String name);
+    public UserInStorage getInfoUserByToken(String token);
+    public String getPass(String name);
+    public boolean updateTokenByIdUser(String idUser, String token);
+    public boolean updateTokenByToken(String oldToken, String newToken);
+    public boolean checkUserName(String name);
+    public boolean checkMail(String mail);
+}
+
---
+++ src/main/java/storage/PropReader.java
@@ -0,0 +1,29 @@
+package storage;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class PropReader {
+    public static String getVal(String key){
+
+        Properties prop=new Properties();
+        String propFileName= "config.properties";
+        InputStream inputStream= PropReader.class.getClassLoader().getResourceAsStream(propFileName);
+
+        if(inputStream != null){
+            try
+            {
+                prop.load(inputStream);
+                return prop.getProperty(key);
+
+            } catch (IOException e)
+            {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        return null;
+    }
+}
